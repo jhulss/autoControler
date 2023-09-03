9dfbab85eaefe2dfc1b2d12ebe8b839c0115a9cf
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-09-03 15:23:05 -0400

    Segunda prueba, al ingresar una cadena vacia, se muestra un mensaje de advertencia

--- parcel-jest-base-main/src/controladorAuto.test.js
+++ parcel-jest-base-main/src/controladorAuto.test.js
@@ -2,8 +2,16 @@ import controlAuto from "./controladorAuto"
 
 
 describe('Mover un auto', () => {
-    it('Al ingresar una cadena vacia, se retorna la misma cadena', () =>{
+    it('Al ingresar una cadena, se retorna la misma cadena', () =>{
         const control = new controlAuto();
         expect(control.mover_auto("hola").toString("hola"))
     })
+
+    it('Al ingresar una cadena vacia, se retorna un mensaje de advertencia', () =>{
+        const control = new controlAuto();
+        expect(control.mover_auto("").toString("Cadena vacia"))
+    })
+    
+
+
 })
